abc27999fde78f052eddf80430c1349d60266a49
ikeawesom <ikelimjl@gmail.com>
2024-04-02 01:07:29 +0800

    added navigation links constants

--- src/constants.ts
+++ src/constants.ts
@@ -1,1 +1,20 @@
 export const DEFAULT_ICON_SIZE = 30;
+
+export type NavLinksType = {
+  title: string;
+  link: string;
+  icon?: string;
+};
+
+export const NAV_LINKS = [
+  { title: "Home", link: "/" },
+  { title: "About Us", link: "/about" },
+  { title: "Products", link: "/products" },
+  { title: "Blog", link: "/blog" },
+  { title: "Contact Us", link: "/contact" },
+] as NavLinksType[];
+
+export const NAV_ACTION_LINKS = [
+  { title: "Cart", link: "/cart", icon: "cart.svg" },
+  { title: "Account", link: "/auth", icon: "user.svg" },
+] as NavLinksType[];
