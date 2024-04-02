b744a827df46e4103e38ce72b0dbc21bc74c7c89
ikeawesom <ikelimjl@gmail.com>
2024-04-02 14:26:22 +0800

    created form for newsletter

---
+++ src/components/sections/newsletter/NewsletterForm.tsx
@@ -0,0 +1,22 @@
+"use client";
+import React, { ChangeEvent, useState } from "react";
+import SecondaryButton from "../../utils/SecondaryButton";
+
+export default function NewsletterForm() {
+  const [email, setEmail] = useState("");
+  return (
+    <form className="flex flex-col items-start justify-start gap-6 w-full">
+      <input
+        value={email}
+        className="p-4 w-full"
+        placeholder="Enter your email here"
+        onChange={(e: ChangeEvent<HTMLInputElement>) =>
+          setEmail(e.target.value)
+        }
+      />
+      <SecondaryButton className="max-[400px]:w-full w-fit hover:translate-x-1">
+        Sign Up
+      </SecondaryButton>
+    </form>
+  );
+}
