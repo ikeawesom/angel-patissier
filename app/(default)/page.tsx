347f12c191b42a2e35af81aef618b841a6c33586
ikeawesom <ikelimjl@gmail.com>
2024-04-02 10:26:57 +0800

    added why us section

--- app/(default)/page.tsx
+++ app/(default)/page.tsx
@@ -1,9 +1,19 @@
+import WhyUsSection from "@/src/components/sections/whyUs/WhyUsSection";
 import Hero from "@/src/components/utils/Hero";
 
 export default function Home() {
   return (
     <>
       <Hero />
+      <div className="w-full grid place-items-center p-10">
+        <div className="w-full max-w-[1000px]">
+          {/* <FeaturedProducts/> */}
+          <WhyUsSection />
+          {/* <ReviewsSection/> */}
+          {/* <FAQSection/> */}
+          {/* <NewsletterSection/> */}
+        </div>
+      </div>
     </>
   );
 }
