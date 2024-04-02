bd8f23fcf07a8e53c000e94858f28b811e37c0a3
ikeawesom <ikelimjl@gmail.com>
2024-04-02 14:26:04 +0800

    organized using wrapper

--- app/(default)/page.tsx
+++ app/(default)/page.tsx
@@ -1,19 +1,22 @@
+import NewsletterSection from "@/src/components/sections/newsletter/NewsletterSection";
 import WhyUsSection from "@/src/components/sections/whyUs/WhyUsSection";
 import Hero from "@/src/components/utils/Hero";
+import Wrapper from "@/src/components/utils/Wrapper";
 
-export default function Home() {
+export default async function Home() {
   return (
     <>
       <Hero />
-      <div className="w-full grid place-items-center p-10">
-        <div className="w-full max-w-[1000px]">
+      <div className="w-full grid place-items-center">
+        <Wrapper className="flex flex-col items-start justify-start gap-20 p-10">
           {/* <FeaturedProducts/> */}
           <WhyUsSection />
           {/* <ReviewsSection/> */}
           {/* <FAQSection/> */}
-          {/* <NewsletterSection/> */}
-        </div>
+        </Wrapper>
       </div>
+
+      {/* <Footer/> */}
     </>
   );
 }
