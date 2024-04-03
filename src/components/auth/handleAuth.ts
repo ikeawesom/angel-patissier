1398aca5ed69c1700523e240e54605a47bf97fe0
ikeawesom <ikelimjl@gmail.com>
2024-04-03 17:18:45 +0800

    auth handler for accounts

--- src/components/auth/handleAuth.ts
+++ src/components/auth/handleAuth.ts
@@ -1,11 +1,55 @@
 "use server";
 
-export async function handleLoginForm(formData: FormData) {
-  console.log(formData);
+import { AuthMemberType } from "@/src/constants";
+import { getSupabaseServer } from "@/src/supabase/server";
+import handleResponses from "@/src/utils/handleResponses";
+import { revalidatePath } from "next/cache";
+
+export async function handleLoginForm(loginDetails: AuthMemberType) {
+  const supabase = getSupabaseServer();
+
+  const email = loginDetails["email"];
+  const password = loginDetails["password"];
+
+  try {
+    const { data, error } = await supabase.auth.signInWithPassword({
+      email,
+      password,
+    });
+    if (error) throw new Error(error.message);
+    revalidatePath("/");
+    return handleResponses({ data });
+  } catch (err: any) {
+    return handleResponses({ status: false, error: err.message });
+  }
 }
 
-export async function handleRegisterForm(formData: FormData) {
-  console.log(formData);
+export async function handleRegisterForm(memberDetails: AuthMemberType) {
+  const supabase = getSupabaseServer();
+
+  const first_name = memberDetails["first_name"];
+  const last_name = memberDetails["last_name"];
+  const email = memberDetails["email"];
+  const password = memberDetails["password"];
+
+  try {
+    const res = await supabase.auth.signUp({
+      email,
+      password,
+      options: {
+        data: {
+          first_name,
+          last_name,
+          role: "member",
+        },
+      },
+    });
+
+    if (res.error) throw new Error(res.error.message);
+    return handleResponses({ data: res.data });
+  } catch (err: any) {
+    return handleResponses({ status: false, error: err.message });
+  }
 }
 
 export async function handleForgotPassword(formData: FormData) {
@@ -13,5 +57,14 @@ export async function handleForgotPassword(formData: FormData) {
 }
 
 export async function handleLogout() {
-  console.log("log out");
+  const supabase = getSupabaseServer();
+
+  try {
+    const { error } = await supabase.auth.signOut();
+    if (error) throw new Error(error.message);
+    revalidatePath("/");
+    return handleResponses();
+  } catch (err: any) {
+    return handleResponses({ status: false, error: err.message });
+  }
 }
