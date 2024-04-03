2b6df445737dd593674898b03bcdf6af258cdefc
ikeawesom <ikelimjl@gmail.com>
2024-04-03 17:20:26 +0800

    auth check for redirect

--- app/(admin)/auth/layout.tsx
+++ app/(admin)/auth/layout.tsx
@@ -1,10 +1,16 @@
 import Back from "@/src/components/navigation/Back";
+import { getSupabaseServer } from "@/src/supabase/server";
+import { redirect } from "next/navigation";
 
-export default function AuthLayout({
+export default async function AuthLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const supabase = getSupabaseServer();
+  const { data } = await supabase.auth.getUser();
+  if (data?.user) redirect("/dashboard");
+
   return (
     <div className="min-h-svh background-auth p-10 flex flex-col items-start justify-start gap-10">
       <Back dark>Go Back</Back>
