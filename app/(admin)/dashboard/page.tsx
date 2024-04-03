784c3f8adc2be0a26e661060a231f2b15676a82d
ikeawesom <ikelimjl@gmail.com>
2024-04-03 17:19:56 +0800

    created dashboard page

--- app/(admin)/dashboard/page.tsx
+++ app/(admin)/dashboard/page.tsx
@@ -1,5 +1,17 @@
 import React from "react";
+import { getSupabaseServer } from "@/src/supabase/server";
+import { redirect } from "next/navigation";
+import LogoutButton from "@/src/components/auth/LogoutButton";
 
-export default function page() {
-  return <div>Dashboard</div>;
+export default async function page() {
+  const supabase = getSupabaseServer();
+  const { data, error } = await supabase.auth.getUser();
+  if (error || !data?.user) redirect("/");
+
+  return (
+    <div>
+      <h1>Welcome {data.user.user_metadata["first_name"]}!</h1>
+      <LogoutButton />
+    </div>
+  );
 }
