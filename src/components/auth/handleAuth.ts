56efd1686c3edd307ff14c9a2b83f824d0cce361
ikeawesom <ikelimjl@gmail.com>
2024-04-03 11:49:50 +0800

    auth server actions

---
+++ src/components/auth/handleAuth.ts
@@ -0,0 +1,17 @@
+"use server";
+
+export async function handleLoginForm(formData: FormData) {
+  console.log(formData);
+}
+
+export async function handleRegisterForm(formData: FormData) {
+  console.log(formData);
+}
+
+export async function handleForgotPassword(formData: FormData) {
+  console.log(formData);
+}
+
+export async function handleLogout() {
+  console.log("log out");
+}
